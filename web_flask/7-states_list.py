99ab5ede063c2288c750953dbc9bd24395690247
Dianah Teta <d.teta@alustudent.com>
2026-06-15 15:42:25 +0200

    Fix web_flask routes 7-9: use State class, clean templates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""starts a Flask web application"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """display a HTML page with states listed alphabetically"""
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
@@ -20,6 +17,7 @@ def states_list():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
